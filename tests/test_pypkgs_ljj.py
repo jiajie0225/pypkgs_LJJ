6b1bf8c5704e33fb1a647d09542336d33a605b84
jiajie0225 <jiajielim97@gmail.com>
2021-02-26 03:32:27 -0800

    added unit tests for catbind

--- tests/test_pypkgs_ljj.py
+++ tests/test_pypkgs_ljj.py
@@ -1,5 +1,14 @@
 from pypkgs_ljj import __version__
 from pypkgs_ljj import pypkgs_ljj
+import pandas as pd
+
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    assert ((pypkgs_ljj.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+    assert ((pypkgs_ljj.catbind(a, b)).categories == ["but", "character",
+            "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
